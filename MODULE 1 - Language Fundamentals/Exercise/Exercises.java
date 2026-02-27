93f7b9930b97e959a4c7e64528cfea5fa3271ef6
Luskapaes5 <paeslucas.amaro@gmail.com>
2026-02-27 05:41:05 -0300

    docs

--- MODULE 1 - Language Fundamentals/Exercise/Exercises.java
+++ MODULE 1 - Language Fundamentals/Exercise/Exercises.java
@@ -1,5 +1,12 @@
 public class Exercises {
+
     public static void main(String[] args){
 
+        int idade = 20;
+        double testosterona = 0.9;
+        char letraInicial = 'L';
+        boolean homo = false;
+        final boolean male = true;
+        
     }
 }
